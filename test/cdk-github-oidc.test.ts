67774b66e07dd4821c10e5adb48c35d6da7728a0
maskerade <stefdefig@hotmail.co.uk>
2022-05-10 16:22:33 +0100

    Initial commit

---
+++ test/cdk-github-oidc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkGithubOidc from '../lib/cdk-github-oidc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-github-oidc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkGithubOidc.CdkGithubOidcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
